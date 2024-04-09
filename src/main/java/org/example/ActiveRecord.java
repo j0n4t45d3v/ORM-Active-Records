449231aacfeaff6274daf485fdccb5773fb44241
Jonatas <jonatas.melo@3lminformatica.com.br>
2024-04-09 13:45:37 -0300

    feat: pegando de forma dinamica o metodo setter e chamando ele

--- src/main/java/org/example/ActiveRecord.java
+++ src/main/java/org/example/ActiveRecord.java
@@ -1,39 +1,79 @@
 package org.example;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
 public abstract class ActiveRecord {
     public static String TABLE_NAME = "";
 
-    public void findAll() {
+    public <T> List<T> findAll() {
         String sql = "SELECT * FROM " + TABLE_NAME;
+        List<T> result = new ArrayList<>();
         try {
             Connection con = Dao.getInstance().getConnection();
             PreparedStatement stmt = con.prepareStatement(sql);
             ResultSet rs = stmt.executeQuery();
 
             while (rs.next()) {
+                T value = (T) this.getClass().newInstance();
                 Field[] fields = this.getClass().getDeclaredFields();
                 for (Field field : fields) {
-                    System.out.println(rs.getString(field.getName()));
+                    Method setMethod = reflectionSetMethod(field);
+
+                     Object valueDb;
+                    if (field.getType().equals(int.class)) {
+                        valueDb = rs.getInt(field.getName());
+                    } else {
+                        valueDb = rs.getString(field.getName());
+                    }
+                    setMethod.invoke(value, valueDb);
                 }
+                result.add(value);
             }
+            return result;
         } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void getFields() {
-        Field[] fields = this.getClass().getDeclaredFields();
-        StringBuilder stringfields = new StringBuilder();
-        for (Field field : fields) {
-            stringfields.append("," + field.getName());
-        }
+    private Method reflectionSetMethod(Field field) throws NoSuchMethodException {
+        String[] splitFieldName = field.getName().split("");
+        splitFieldName[0] = splitFieldName[0].toUpperCase();
+        String fieldNameCameOnCase = String.join("", splitFieldName);
+        Class<? extends ActiveRecord> clazz = this.getClass();
+        Class<?> paramType = field.getType();
+        String methodName = "set" + fieldNameCameOnCase;
+        return clazz.getMethod(methodName, paramType);
+    }
 
-        System.out.println(stringfields.toString().substring(1) + ", TABELA: " + TABLE_NAME);
+    public String getFields() {
+        Field[] fields = this.getClass().getDeclaredFields();
+        StringBuilder stringFields = new StringBuilder();
+        for (Field field : fields) {
+            stringFields.append(",").append(field.getName());
+        }
+        return stringFields.substring(1);
+    }
+
+    @Override
+    public String toString() {
+        Field[] fields = this.getClass().getDeclaredFields();
+        return this.getFields();
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,18 @@
 package org.example;
 
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class Main {
     private final static Logger LOG = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
        Teste teste = new Teste();
 
-       teste.findAll();
+       List<Teste> teste2 = teste.findAll();
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(teste2));
     }
 }
